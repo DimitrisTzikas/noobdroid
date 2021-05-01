d5be73941cd5c9d3ca4b1692c19037f229dada94
DimitrisTzikas <dimitristzikas@protonmail.com>
2021-05-01 17:07:31 +0300

    Make LocalDB Serializable

--- app/src/main/java/gr/ihu/noobdroid/LocalDB/LocalDB.java
+++ app/src/main/java/gr/ihu/noobdroid/LocalDB/LocalDB.java
@@ -9,8 +9,10 @@ import androidx.annotation.Nullable;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import java.io.Serializable;
+
 @Database(entities = {Sport.class, Sportsman.class, Team.class}, version = 1)
-public abstract class LocalDB extends RoomDatabase {
+public abstract class LocalDB extends RoomDatabase implements Serializable {
 
     public abstract LocalDBInterface localDBInterface();
 
---
+++ app/src/main/java/gr/ihu/noobdroid/SportInsertFragment.java
@@ -0,0 +1,28 @@
+package gr.ihu.noobdroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.room.Room;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gr.ihu.noobdroid.LocalDB.LocalDB;
+
+public class sportInsertFragment extends Fragment {
+
+    public static LocalDB localDB;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sport_insert, container, false);
+    }
+}
