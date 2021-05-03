3b808d2907c0713fa05f9ca66fc4ec48965a22b1
nikos dimitriou <it073275@it.teithe.gr>
2021-05-03 15:09:24 +0300

    firestore changes

--- app/src/main/java/gr/ihu/noobdroid/MainActivity.java
+++ app/src/main/java/gr/ihu/noobdroid/MainActivity.java
@@ -1,14 +1,31 @@
 package gr.ihu.noobdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static FragmentManager fragmentManager;
+    public static FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+        fragmentManager = getSupportFragmentManager();
+        if(findViewById(R.id.fragment_container)!=null){
+            if(savedInstanceState!=null){
+                return;
+            }
+            fragmentManager.beginTransaction().add(R.id.fragment_container,new MainFragment()).commit();
+        }
     }
 
 }
