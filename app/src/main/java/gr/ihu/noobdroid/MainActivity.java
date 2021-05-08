b092c459e69d18cd3a1e8de497c701a2f546addf
DimitrisTzikas <dimitristzikas@protonmail.com>
2021-05-09 00:10:24 +0300

    Update UI

--- app/src/main/java/gr/ihu/noobdroid/MainActivity.java
+++ app/src/main/java/gr/ihu/noobdroid/MainActivity.java
@@ -1,13 +1,17 @@
 package gr.ihu.noobdroid;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.muddzdev.styleabletoast.StyleableToast;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.mainFragment,
                 R.id.sportFragment,
                 R.id.sportsmanFragment,
                 R.id.teamFragment
@@ -44,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                new StyleableToast
+                        .Builder(this)
+                        .text("Settings")
+                        .textColor(Color.WHITE)
+                        .backgroundColor(Color.GREEN)
+                        .show();
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
